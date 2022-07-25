01bdb7a5ade49f3607e133f473f4b2b8e3084454
NandaCardinaly <fernanda.cardinaly@gmail.com>
2022-07-25 07:37:24 -0300

    consumindo api da Imdb, carregando a imagem, transformando em png, escrevendo texto e gerando uma sticker do whatsapp.

--- Exercitando/src/treinando/estruturadeSelecao/EstruturaIfElse.java
+++ Exercitando/src/treinando/estruturadeSelecao/EstruturaIfElse.java
@@ -18,40 +18,5 @@ public class EstruturaIfElse {
     public static void main(String[] args) {
 
 
-    /*Escreva um	 programa que	 leia	 as	 medidas	 dos	 lados	 de	 um	 triângulo	 e
-    escreva	se	ele	é	Equilátero,	Isósceles	ou	Escaleno.	Sendo	que:
-    − Triângulo	Equilátero:	possui	os	3	lados	iguais.
-    − Triângulo	Isóscele:	possui	2	lados	iguais.
-    − Triângulo	Escaleno:	possui	3	lados	diferentes. */
-
-
-        Scanner ler = new Scanner(System.in);
-        System.out.println("Digite o primeiro lado: ");
-        double ladoA = ler.nextDouble();
-        System.out.println("Digite o segundo lado: ");
-        double ladoB = ler.nextDouble();
-        System.out.println("Digite o terceiro lado: ");
-        double ladoC = ler.nextDouble();
-
-        if ((ladoA == ladoB) && (ladoB == ladoC) ) {
-
-            System.out.println("Esse Triângulo é Equilátero: pois possui todos os lados iguais. ");
-
-        }
-
-        else{
-            if ((ladoA == ladoB && ladoA != ladoB) || (ladoA == ladoC && ladoA != ladoB) || (ladoC == ladoB && ladoC!= ladoA )){
-                System.out.println("Esse Triângulo é Isóscele: pois possui 2 lados iguais. ");
-
-            }
-
-            else{
-                System.out.println("Esse Triângulo é Escaleno: pois possui todos os lados diferentes. ");
-            }
-
-        }
-
-
+        S
     }
-}
-
