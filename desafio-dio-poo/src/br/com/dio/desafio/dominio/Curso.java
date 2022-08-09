77dde0bddec2ce6d00aea3f2a03b8fd07d44026c
NandaCardinaly <fernanda.cardinaly@gmail.com>
2022-08-08 22:20:30 -0300

    Criacao da classe progenitora mae e as classes mentoria e curso passam a serem herdeiras

--- desafio-dio-poo/src/br/com/dio/desafio/dominio/Curso.java
+++ desafio-dio-poo/src/br/com/dio/desafio/dominio/Curso.java
@@ -1,29 +1,13 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo {
+
     private int cargaHoraria;
 
     public Curso() {
     }
 //getters e setters
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public int getCargaHoraria() {
         return cargaHoraria;
@@ -36,9 +20,14 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", cargaHoraria=" + cargaHoraria +
                 '}';
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
 }
