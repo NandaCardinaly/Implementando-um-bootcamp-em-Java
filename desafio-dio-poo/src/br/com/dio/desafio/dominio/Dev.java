f36e1e66be4e5af6bad91c0d1d964e98b047862b
NandaCardinaly <fernanda.cardinaly@gmail.com>
2022-08-09 10:29:49 -0300

    Criacao da classe progenitora mae e as classes mentoria e curso passam a serem herdeiras

---
+++ desafio-dio-poo/src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,49 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>(); // o linked hashset é para ficar na ordem e sem repetir o curso ou mentoria
+
+
+    public void inscreverBootcamp(Bootcamp bootcamp){
+
+    }
+
+    public void progredir(){
+
+
+    }
+
+    public void calcularXP(){
+
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+}
