bbd794967da67231a7e033eeb6a3feb3847fa6b2
NandaCardinaly <fernanda.cardinaly@gmail.com>
2022-08-10 16:14:45 -0300

    Interliguei todas as classes de dominio

--- desafio-dio-poo/src/br/com/dio/desafio/dominio/Dev.java
+++ desafio-dio-poo/src/br/com/dio/desafio/dominio/Dev.java
@@ -1,6 +1,8 @@
 package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -10,15 +12,26 @@ public class Dev {
 
 
     public void inscreverBootcamp(Bootcamp bootcamp){
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+
 
     }
 
     public void progredir(){
+        Optional<Conteudo> conteudo =  this.conteudosInscritos.stream().findFirst();
+        if(conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.out.println("Você não está matriculado em nenhum conteúdo");
+        }
 
 
     }
 
-    public void calcularXP(){
+    public double calcularXP(){
+        return this.conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
 
 
     }
@@ -46,4 +59,19 @@ public class Dev {
     public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
         this.conteudosConcluidos = conteudosConcluidos;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
+
+
 }
